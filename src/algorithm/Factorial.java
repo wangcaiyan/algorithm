7108f897fb540504cba5fd76baea8ec4d17258ea
wang_cy1 <wang_cy1@suixingpay.com>
2019-05-21 18:45:17 +0800

    阶乘算法

---
+++ src/algorithm/Factorial.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+/**
+ * 递归计算阶乘
+ * 
+ * @author wangcaiyan
+ *
+ */
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		int result = factorial(num);
+		System.out.println(num + "的阶乘是" + result);
+
+	}
+
+	/**
+	 * 递归计算阶乘
+	 * 
+	 * @param num
+	 * @param result
+	 * @return
+	 */
+	public static int factorial(int num) {
+		if (0 < num) {
+			return num * factorial(num - 1);
+		} else {
+			return 1;
+		}
+	}
+
+}
